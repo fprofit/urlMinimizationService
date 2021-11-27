71d79bcdf955857f4c52ecf781a0c106b4ba8639
fprofit <mix.rec.rm@gmail.com>
2021-11-27 13:43:20 +0300

    add method get & post & func UrlGenerator

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	//"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,5 +15,34 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/getMinimizedUrlToOriginalUrl", func(c *gin.Context) {
+		c.String(200, func(minimizedUrl string) (originalUrl string) {
+			originalUrl = minimizedUrl
+			return
+		}("get"))
+	})
+
+	r.POST("/postOriginalUrlToMinimizedUrl", func(c *gin.Context) {
+		c.String(200, func(originalUrl string) (minimizedUrl string) {
+			minimizedUrl = originalUrl
+			return
+		}("post"))
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func UrlGenerator(originalUrl string) (minimizedUrl string) {
+
+	// if originalUrl == urlToDB{
+	// 	minimizedUrl = urlToDB[originalUrl]
+	// 	return
+	// }
+
+	symbol := "1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	for i := 0; i < 10; i++ {
+		rand.Seed(time.Now().UTC().UnixNano() % 10000)
+		minimizedUrl += string(symbol[rand.Intn(62)])
+	}
+	return
 }
