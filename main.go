da850a26c737f33d51cd2d5715b8b960d7415be8
fprofit <mix.rec.rm@gmail.com>
2021-11-27 21:42:01 +0300

    add func checkUrl refact UrlGen ticker & post

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	//"fmt"
 	"math/rand"
+	"regexp"
+	"strings"
 	"time"
+
+	//"sync/atomic"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	r.GET("/getMinimizedUrlToOriginalUrl", func(c *gin.Context) {
 		c.String(200, func(minimizedUrl string) (originalUrl string) {
 			originalUrl = minimizedUrl
@@ -23,10 +22,14 @@ func main() {
 	})
 
 	r.POST("/postOriginalUrlToMinimizedUrl", func(c *gin.Context) {
-		c.String(200, func(originalUrl string) (minimizedUrl string) {
-			minimizedUrl = originalUrl
-			return
-		}("post"))
+		originalUrl := c.Query("originalUrl")
+		minimizedUrl := UrlGenerator(checkUrl(originalUrl))
+		if minimizedUrl != "false" {
+			c.String(200, func(minimizedUrl string) (originalUrl string) {
+				originalUrl = minimizedUrl
+				return
+			}(minimizedUrl))
+		}
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
@@ -41,8 +44,27 @@ func UrlGenerator(originalUrl string) (minimizedUrl string) {
 
 	symbol := "1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	for i := 0; i < 10; i++ {
-		rand.Seed(time.Now().UTC().UnixNano() % 10000)
+		time.Sleep(42 * time.Millisecond)
+		rand.Seed(time.Now().UTC().UnixNano())
 		minimizedUrl += string(symbol[rand.Intn(62)])
 	}
 	return
+}
+
+func checkUrl(url string) string {
+	var validUrl = regexp.MustCompile(`(https?:\/\/)?([\w-]{1,32}\.[a-zA-Z]{2,32})[^\s]*`)
+	var validHttp = regexp.MustCompile(`(https?:\/\/)([\w-]{1,32}\.[a-zA-Z]{2,32})[^\s]*`)
+	var validWww = regexp.MustCompile(`(www.)([\w-]{1,32}\.[a-zA-Z]{2,32})[^\s]*`)
+	if validUrl.MatchString(url) {
+		if validHttp.MatchString(url) {
+			strHttp := strings.Split(url, "://")
+			url = strHttp[1]
+		}
+		if validWww.MatchString(url) {
+			strWww := strings.Split(url, "www.")
+			url = strWww[1]
+		}
+		return url
+	}
+	return "false"
 }
