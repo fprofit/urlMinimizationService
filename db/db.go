620dc50457ea53b740405cdb35c76dcc076323e6
fprofit <mix.rec.rm@gmail.com>
2021-11-29 18:33:40 +0300

    create db

---
+++ db/db.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	//"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func dbtest() {
+	fmt.Println("ok")
+
+	// connString := `user=postgres           // username to access the database
+	// password=1234           // password to access the database
+	// host=localhost          // default host of PostgreSQL
+	// port=5432               // default port of PostgreSQL
+	// dbname=peopleDatabase   // name of PostgreSQL database
+	// sslmode=disable`
+}
