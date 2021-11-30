09baf71f6d7f15b6a967ec78539697e62441ac15
fprofit <mix.rec.rm@gmail.com>
2021-11-30 14:06:38 +0300

    add flag & fn inmemory & funcBDSQL  & fn fof db

--- db.go
+++ db.go
@@ -3,21 +3,51 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/lib/pq"
 )
 
-func dbw() string {
+var psqlInfo string
 
-	connString := "user=postgres password=MiX7681726 host=localhost port=5432 sslmode=disable"
-
-	db, err := sql.Open("postgres", connString)
+func getMiniToOrig(origUrl string) (miniUrl string) {
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal(err)
-		defer db.Close()
-		fmt.Println(db)
+		return "false"
 	}
+	defer db.Close()
 
-	return "ok"
+	s := fmt.Sprintf("SELECT miniUrl FROM miniurltorigrurl_table WHERE origUrl='%s';", origUrl)
+	err = db.QueryRow(s).Scan(&miniUrl)
+	if err != nil {
+		miniUrl = UrlGenerator(origUrl)
+
+		insDB := `
+		INSERT INTO miniurltorigrurl_table (miniUrl, origUrl)
+		VALUES($1, $2)`
+		_, err := db.Exec(insDB, miniUrl, origUrl)
+		if err != nil {
+			miniUrl = "false"
+		}
+	}
+	return
 }
+
+func getOrigToMini(miniUrl string) (origUrl string) {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return "false"
+	}
+	defer db.Close()
+
+	s := fmt.Sprintf("SELECT origUrl FROM miniurltorigrurl_table WHERE miniUrl='%s';", miniUrl)
+	err = db.QueryRow(s).Scan(&origUrl)
+	if err != nil {
+		return "false"
+	}
+	return
+}
+
+// CREATE TABLE miniurltorigrurl_table (
+// 	miniUrl varchar(10) UNIQUE NOT NULL,
+//  origUrl varchar NOT UNIQUE NULL,
+// PRIMARY KEY(miniUrl, origUrl));
--- main.go
+++ main.go
@@ -13,35 +13,31 @@ import (
 var (
 	inMemory = make(map[string]string)
 	validUrl = regexp.MustCompile(`^(https?:\/\/)?[\w]{1,32}\.[a-zA-Z]{2,32}[^\s]*$`)
+	rGet     = "/getMinimizedUrlToOriginalUrl"
+	rPost    = "/postOriginalUrlToMinimizedUrl"
 )
 
 func main() {
-
-	//router()
-
 	inMem := flag.Bool("inMemory", false, "Run microService in memory")
-	dbwSQL := flag.String("dbSQL", "", "Run DB SQL")
+	conBDbSQL := flag.String("dbSQL", "", "Run DB SQL")
 	flag.Parse()
 
 	if *inMem {
 		fmt.Println("inMemory", *inMem)
-	} else if *dbwSQL != "" {
-		fmt.Println("dbSQL", *dbwSQL)
+		inMemoryFunc()
+	} else if *conBDbSQL != "" {
+		psqlInfo = *conBDbSQL
+		funcBDSQL()
 	}
-	fmt.Println(dbw())
-
 }
 
-func router() {
+func funcBDSQL() {
+
 	r := gin.Default()
-	r.GET("/getMinimizedUrlToOriginalUrl", func(c *gin.Context) {
+	r.GET(rGet, func(c *gin.Context) {
 		minimizedUrl := c.Query("minimizedUrl")
-		originalUrl := ""
-		for origUrl, miniUrl := range inMemory {
-			if minimizedUrl == miniUrl {
-				originalUrl = origUrl
-			}
-		}
+		originalUrl := getOrigToMini(minimizedUrl)
+
 		if originalUrl != "" {
 			c.String(200, originalUrl)
 		} else {
@@ -49,10 +45,49 @@ func router() {
 		}
 	})
 
-	r.POST("/postOriginalUrlToMinimizedUrl", func(c *gin.Context) {
+	r.POST(rPost, func(c *gin.Context) {
 		originalUrl := c.Query("originalUrl")
 		if validUrl.MatchString(originalUrl) {
-			c.String(200, UrlGenerator(originalUrl))
+			c.String(200, getMiniToOrig(originalUrl))
+
+		} else {
+			c.String(400, "")
+		}
+	})
+
+	r.Run()
+}
+
+func inMemoryFunc() {
+
+	r := gin.Default()
+	r.GET(rGet, func(c *gin.Context) {
+		minimizedUrl := c.Query("minimizedUrl")
+		originalUrl := ""
+		_, ok := inMemory[minimizedUrl]
+		if ok {
+			originalUrl = inMemory[minimizedUrl]
+			c.String(200, originalUrl)
+		} else {
+			c.String(400, "")
+		}
+	})
+
+	r.POST(rPost, func(c *gin.Context) {
+		originalUrl := c.Query("originalUrl")
+		if validUrl.MatchString(originalUrl) {
+			miniUrlMap := ""
+			for miniUrl, origUrl := range inMemory {
+				if origUrl == originalUrl {
+					miniUrlMap = miniUrl
+				}
+			}
+			if miniUrlMap == "" {
+				miniUrlMap = UrlGenerator(originalUrl)
+				inMemory[miniUrlMap] = originalUrl
+			}
+			c.String(200, miniUrlMap)
+
 		} else {
 			c.String(400, "")
 		}
@@ -62,20 +97,11 @@ func router() {
 }
 
 func UrlGenerator(originalUrl string) (minimizedUrl string) {
-
-	_, ok := inMemory[originalUrl]
-	if ok {
-		return inMemory[originalUrl]
-	}
-
 	symbol := "1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	for i := 0; i < 10; i++ {
 		time.Sleep(42 * time.Millisecond)
 		rand.Seed(time.Now().UTC().UnixNano())
 		minimizedUrl += string(symbol[rand.Intn(62)])
 	}
-
-	inMemory[originalUrl] = minimizedUrl
-
 	return
 }
